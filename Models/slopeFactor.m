4c884cdf611f7cbcdcec276125b5cba57b354af1
Camilla Stokkevaag <camilla.stokkevag@ift.uib.no>
2013-11-09 23:16:32 +0100

    Added ERR and EAR model parameters

--- Models/slopeFactor.m
+++ Models/slopeFactor.m
@@ -5,6 +5,8 @@ function result = slopeFactor(cancerSite, gender, exposureAge, age)
 % exposureAge is the age of patient at exposure.
 % age is the attained age.
 
+% eksample: a = 10:40; plot(a, slopeFactor('BEIR_EAR_Lung', 'F', 10, a));
+
 [beta, gamma, eta, tau, c1, c2, c3, c4, k1, k2] = selectParams(cancerSite, gender);
 % Calculating:
 %   beta * exp(gamma * e_star + eta * ln(a_star))
@@ -30,9 +32,30 @@ return;
 function [beta, gamma, eta, tau, c1, c2, c3, c4, k1, k2] = selectParams(cancerSite, gender)
 
 % Update data table as needed:
-%                                  beta_M  beta_F  gamma  eta   tau  c1    c2  c3  c4  k1    k2
-table = struct('BEIR_VII_Lung',   [0.32    1.4     -0.3   -1.4  30   1/10  -3  0   0   1/60  0  ],
-               'BEIR_VII_Breast', [0       0.51     0     -2    0    1      0  1   0   1/60  0  ]);
+%                                  beta_M  beta_F    gamma   eta  tau  c1    c2  c3  c4  k1    k2
+table = struct('BEIR_ERR_Stomach',  [0.21    0.48    -0.3   -1.4  30   1/10  -3  0   0   1/60  0  ],
+		'BEIR_ERR_Colon',   [0.63    0.43    -0.3   -1.4  30   1/10  -3  0   0   1/60  0  ],
+		'BEIR_ERR_Liver',   [0.32    0.32    -0.3   -1.4  30   1/10  -3  0   0   1/60  0  ],
+		'BEIR_ERR_Lung',    [0.32    1.4     -0.3   -1.4  30   1/10  -3  0   0   1/60  0  ],
+		'BEIR_ERR_Breast',  [0       0.51     0     -2     0   1      0  1   0   1/60  0  ],
+		'BEIR_ERR_Prostate',[0.12    0       -0.3   -1.4  30   1/10  -3  0   0   1/60  0  ], 
+		'BEIR_ERR_Uterus',  [0       0.055   -0.3   -1.4  30   1/10  -3  0   0   1/60  0  ], 
+		'BEIR_ERR_Ovary',   [0       0.38    -0.3   -1.4  30   1/10  -3  0   0   1/60  0  ], 
+		'BEIR_ERR_Bladder', [0.50    1.65    -0.3   -1.4  30   1/10  -3  0   0   1/60  0  ], 
+		'BEIR_ERR_Thyroid', [0.53    1.05    -0.83   0     0   1      0  1   0   1/60  0  ],
+	'BEIR_ERR_OtherSolidCancer',[0.27    0.45    -0.3   -2.8  30   1/10  -3  0   0   1/60  0  ],
+
+               'BEIR_EAR_Stomach',  [4.9     4.9     -0.41    2.8  30   1/10  -3  0   0   1/60  0  ],
+		'BEIR_EAR_Colon',   [3.2     0.43    -0.41    2.8  30   1/10  -3  0   0   1/60  0  ],
+		'BEIR_EAR_Liver',   [2.2     0.32    -0.41    4.1  30   1/10  -3  0   0   1/60  0  ],
+		'BEIR_EAR_Lung',    [2.3     1.4     -0.41    5.2  30   1/10  -3  0   0   1/60  0  ],
+		'BEIR_EAR_Breast',  [0       0.51    -0.51    3.5   0   1      0  1   0   1/60  0  ],
+		'BEIR_EAR_Prostate',[0.11    0       -0.41    2.8  30   1/10  -3  0   0   1/60  0  ], 
+		'BEIR_EAR_Uterus',  [0       0.055   -0.41    2.8  30   1/10  -3  0   0   1/60  0  ], 
+		'BEIR_EAR_Ovary',   [0       0.38    -0.41    2.8  30   1/10  -3  0   0   1/60  0  ], 
+		'BEIR_EAR_Bladder', [1.20    1.65    -0.41    6.0  30   1/10  -3  0   0   1/60  0  ], 
+		'BEIR_EAR_Thyroid', [0	     0        0       0     0   0      0  0   0    0    0  ],
+	'BEIR_EAR_OtherSolidCancer',[6.2     4.8     -0.41    2.8  30   1/10  -3  0   0   1/60  0  ]); 
 
 if ! isfield(table, cancerSite)
 	error('Invalid cancerSite value "%s".', cancerSite);

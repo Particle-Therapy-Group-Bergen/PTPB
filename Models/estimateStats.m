2e983acfa0dba6aefa74f23d1f1733b97baa9ec2
Artur Szostak <artursz@iafrica.com>
2014-03-31 21:47:42 +0200

    Adding quantile calculation to statistic estimation.

--- Models/estimateStats.m
+++ Models/estimateStats.m
@@ -1,20 +1,31 @@
-function result = estimateStats(x, nsamples)
-%result = estimateStats(x, nsamples)
+function [result, samples] = estimateStats(x, nsamples, lower_quantile, upper_quantile)
+%[result, samples] = estimateStats(x, nsamples, lower_quantile, upper_quantile)
 %
-% Calculates the minimum, maximum, mean, median and standard deviation of a
-% distribution using the bootstrapping method. i.e resampling the distribution
-% with replacement for 'nsamples' number of times to get a distribution of
-% the above mentioned statistics. From these re-samples we can estimate the
-% value of the statistics themselves and their uncertainties.
+% Calculates the minimum, maximum, lower quantile, upper quantile, mean, median
+% and standard deviation of a distribution using the bootstrapping method. i.e
+% resampling the distribution with replacement for 'nsamples' number of times
+% and calculating the statistics on the new distribution. This gives us a
+% number of samples for each statistic which itself forms a distribution. From
+% these resampled distributions we can estimate the value of the statistics and
+% their uncertainties.
+% Note: by default the upper and lower quantile are chosen to form a 95%
+% confidence interval.
 %
 % Parameters:
 %   x - The data points of the distribution.
 %   nsamples - The number of times to resample the data points x. The more
 %              times one resamples the distribution the more precision is
-%              achieved in the final results.
+%              achieved in the final results. (optional, default = 1000)
+%   lower_quantile - The lower quantile value in the range [0..1].
+%                    (optional, default = 0.025)
+%   upper_quantile - The upper quantile value in the range [0..1].
+%                    (optional, default = 0.975)
 %
 % The results are returned as a structure with each field representing one
 % of the statistics estimated for the distribution x.
+% In addition, the calculated samples are returned as a Nx7 matrix with the 7
+% columns indicating, the minimum, maximum, lower quantile, upper quantile,
+% mean, median and standard deviation in that order.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -49,20 +60,28 @@ end
 if ~ exist('nsamples')
   nsamples = 1000;
 end
+if ~ exist('lower_quantile')
+  lower_quantile = 2.5e-2;
+end
+if ~ exist('upper_quantile')
+  upper_quantile = 97.5e-2;
+end
 
 xlen = length(x);
 
 samples = [];
 for n = 1:nsamples
   y = x( round(rand(1, xlen) * xlen + 0.5) );
-  samples(n,1:5) = [min(y), max(y), mean(y), median(y), std(y)];
+  q1 = quantile(y, lower_quantile, 1, 8);
+  q2 = quantile(y, upper_quantile, 1, 8);
+  samples(n,1:7) = [min(y), max(y), q1, q2, mean(y), median(y), std(y)];
 end
 
 ymean = mean(samples);
 ysigma = std(samples) ./ sqrt(nsamples);
-field = {'min', 'max', 'mean', 'median', 'stdev'};
+field = {'min', 'max', 'lower_quantile', 'upper_quantile', 'mean', 'median', 'stdev'};
 
 result = struct;
-for n = 1:5
+for n = 1:length(field)
   result.(field{n}) = struct('value', ymean(n), 'uncertainty', ysigma(n));
 end

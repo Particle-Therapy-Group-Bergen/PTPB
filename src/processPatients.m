657eb2a8ccc952130bb137fde086454fa670d4dd
Artur Szostak <artursz@iafrica.com>
2015-02-04 23:27:22 +0100

    Adding first version of code for more general processing of patient files.

---
+++ src/processPatients.m
@@ -0,0 +1,174 @@
+function result = processPatients(dvhfiles, params, organs, models, organ_name_map)
+%result = processPatients(dvhfiles [, params, organs, models, organ_name_map])
+%
+% This is the top level entry point for performing standard model calculations
+% on one or more patient files. The output will be returned as a structure
+% containing the results and sampled uncertainty distributions.
+%
+%Parameters:
+%
+% dvhfiles - A cell array of file paths containing DVH data converted to Matlab
+%            format (.mat) with the convertDVHtoMatlabFile.sh script.
+%
+% params - A structure containing various parameters to use for the calculation.
+%
+% organs - A cell array of organ name strings for which to make calculations.
+%
+% models - A cell array of response models to use in the calculations.
+%
+% organ_name_map - The organ name remapping table passed to the function
+%                  getDoseVolumeHistogram. Refer to that function for details
+%                  about this parameters structure.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%    Particle Therapy Project Bergen (PTPB) - tools and models for research in
+%    cancer therapy using particle beams.
+%
+%    Copyright (C) 2015 Particle Therapy Group Bergen
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Author: Artur Szostak <artursz@iafrica.com>
+
+if nargin == 0
+    % Print help message if no arguments are given.
+    help processPatients;
+    return;
+end
+
+N = 2;
+
+if ~ exist('dvhfiles')
+    error('No file names given for "dvhfiles" cell array.');
+end
+if ~ exist('params') || length(params) == 0
+    params = setupDefaultParams();
+end
+if ~ exist('organs') || length(organs) == 0
+    organs = {};
+end
+if ~ exist('models') || length(models) == 0
+    models = {'LNT', 'PlateauHall', 'LinExp'};
+end
+if ~ exist('organ_name_map')
+    organ_name_map = {};
+end
+
+% Check that we have the necessary fields in the params structure.
+fields = {};
+fields{1} = 'dose_binning_uncertainty_model';
+fields{2} = 'dose_binning_uncertainty';
+fields{3} = 'volume_ratio_uncertainty_model';
+fields{4} = 'volume_ratio_uncertainty';
+fields{5} = 'integrations_methods';
+fields{6} = 'interpolation_methods';
+fields{7} = 'organs';
+for n = 1:length(fields)
+    if ~ isfield(params, fields{n})
+        error('Missing field "%s" in the params structure.', fields{n});
+    end
+end
+
+integrationMethods = params.integrations_methods;
+interpolationMethods = params.interpolation_methods;
+
+result = {};
+for n = 1:length(dvhfiles)
+    filename = dvhfiles{n};
+    result{n} = struct('filename', filename);
+    dvhs = getDoseVolumeHistogram(filename, organ_name_map, params, organs{:});
+    organ_names = fieldnames(dvhs);
+    if length(organs) > 0
+        organ_names = organs;
+    end
+    for m = 1:length(organ_names)
+        organ = organ_names{m};
+        dvh = dvhs.(organ);
+        dvh.dose_binning_uncertainty_model = params.dose_binning_uncertainty_model;
+        dvh.volume_ratio_uncertainty_model = params.volume_ratio_uncertainty_model;
+        if ~ isfield(params.organs, organ)
+            warning('Missing organ "%s" in input parameters "params.organs".', organ);
+            continue;
+        end
+        organParams = params.organs.(organ);
+        for k = 1:length(models)
+            model = models{k};
+            if ~ isfield(organParams, model)
+                warning('Missing model "%s" in input parameters "params.organs.%s".',
+                        model, organ);
+                continue;
+            end
+            modelParams = organParams.(model);
+            if ~ iscell(modelParams)
+                error('Expected a cell array of model parameters for "params.organs.%s.%s".',
+                      organ, model);
+            end
+            oeds = sampleOED(dvh, N, model, integrationMethods,
+                             interpolationMethods, modelParams{:});
+            result{n}.organs.(organ).(model) = oeds;
+        end
+    end
+end
+return;
+
+
+function params = setupDefaultParams()
+% Setup a set of default parameters to be used by this script.
+
+trapzMethod = struct('name', 'trapz', 'tolerance', 1e-6);
+quadMethod = struct('name', 'quad', 'tolerance', sqrt(eps));
+quadvMethod = struct('name', 'quadv', 'tolerance', 1e-6);
+quadlMethod = struct('name', 'quadl', 'tolerance', eps);
+quadgkMethod = struct('name', 'quadgk', 'tolerance', 1e-10);
+integrationMethods = {trapzMethod, quadMethod, quadvMethod, quadlMethod, quadgkMethod};
+interpolationMethods = {'linear', 'pchip', 'cubic', 'spline'};
+
+organParams = struct;
+p = getParameters('plateau_threshold', 'linexp_alphas');
+names = fieldnames(p);
+for n = 1:length(names)
+    name = names{n};
+    organParams.(name).LNT = {};  % Create default entry for LNT.
+    if isfield(p.(name), 'plateau_threshold')
+        A = p.(name).plateau_threshold.range_low;
+        C = p.(name).plateau_threshold.value;
+        B = p.(name).plateau_threshold.range_high;
+        model = struct('uncertainty_model', 'triangle',
+                       'params', {{A, C, B}});
+        model2 = struct('uncertainty_model', 'triangle',
+                       'params', {{1,2,3}});
+        organParams.(name).PlateauHall = {model, model2};
+    end
+    if isfield(p.(name), 'linexp_alpha')
+        A = p.(name).linexp_alpha.range_low;
+        C = p.(name).linexp_alpha.value;
+        B = p.(name).linexp_alpha.range_high;
+        model = struct('uncertainty_model', 'triangle',
+                       'params', {{A, C, B}});
+        organParams.(name).LinExp = {model};
+    end
+end
+
+p = getParameters('histogram_uncertainty');
+params = struct('dose_binning_uncertainty_model', 'box',
+                'dose_binning_uncertainty', p.dose_binning_uncertainty,
+                'volume_ratio_uncertainty_model', 'box',
+                'volume_ratio_uncertainty', p.volume_ratio_uncertainty,
+                'integrations_methods', {integrationMethods},
+                'interpolation_methods', {interpolationMethods},
+                'organs', organParams);
+return;
---
+++ src/sampleOED.m
@@ -0,0 +1,110 @@
+function result = sampleOED(dvh, N, responseModel, integrationMethods, interpolationMethods, varargin)
+%result = sampleOED(dvh, N, responseModel, integrationMethods, interpolationMethods, ...)
+%
+% This function randomly samples input dose volume histogram and response model
+% parameter uncertainty distributions to calculate OED values. This monte-carlo
+% method produces an output distribution of OED samples from which statistical
+% values can be calculated, such as the likely OED value and confidence
+% intervals.
+%
+%Parameters:
+%
+% dvh - The dose volume histogram structure as returned by the
+%       getDoseVolumeHistogram function.
+%
+% N - The number of samples to produce.
+%
+% responseModel - A string indicating the response model to use. Refer to the
+%                 OED function for details.
+%
+% integrationMethods - A cell array of strings naming the integration methods to
+%                      use for the OED function. Will randomly select between
+%                      methods before calculating each new OED sample.
+%
+% interpolationMethods - A cell array of strings naming the interpolation
+%                        methods to use for the OED function. Will randomly
+%                        select between methods before calculating each new OED
+%                        sample.
+%
+% Additional parameters should be structures, one for each response model
+% parameter that must be passed to the OED function. Each structure contains
+% the following two fields to define the uncertainty distribution of the
+% parameter:
+%   uncertainty_model - A string indicating a distribution model to use. Can be
+%                       any value that is accepted by the function
+%                       sampleDistribution.
+%   params - A cell array of parameters passed to sampleDistribution that define
+%            the distribution. The exact number of parameters and their meaning
+%            depends on the value of uncertainty_model chosen. For details refer
+%            to the sampleDistribution function.
+%
+% This function returns a vector of OED samples.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%    Particle Therapy Project Bergen (PTPB) - tools and models for research in
+%    cancer therapy using particle beams.
+%
+%    Copyright (C) 2015 Particle Therapy Group Bergen
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Author: Artur Szostak <artursz@iafrica.com>
+
+if nargin == 0
+    % Print help message if no arguments are given.
+    help sampleOED;
+    return;
+end
+
+% Sample the DVH N times.
+dose = dvh.datapoints(:,1);
+doseLow = dvh.range_low(:,1);
+doseHigh = dvh.range_high(:,1);
+volume = dvh.datapoints(:,2);
+volumeLow = dvh.range_low(:,2);
+volumeHigh = dvh.range_high(:,2);
+dose_samples = sampleDistribution(dvh.dose_binning_uncertainty_model, N, doseLow', doseHigh');
+volume_samples = sampleDistribution(dvh.volume_ratio_uncertainty_model, N, volumeLow', volumeHigh');
+
+% Sample the response model parameters N times.
+if length(varargin) > 0
+    modelParams = [];
+    for n = 1:length(varargin)
+        p = varargin{n};
+        modelParams(:,n) = sampleDistribution(p.uncertainty_model, N, p.params{:});
+    end
+else
+    modelParams = zeros(N, 0);
+end
+
+result = zeros(N, 1);
+for n = 1:N
+    dvh_samples = [dose_samples(n,:); volume_samples(n,:)]';
+    model_param_samples = mat2cell(modelParams(n,:), 1, ones(length(varargin), 1));
+
+    % Randomly select the integration and interpolation method.
+    index = randi(length(integrationMethods));
+    integrationMethod = integrationMethods{index};
+    index = randi(length(interpolationMethods));
+    interpolationMethod = interpolationMethods{index};
+
+    options = struct('integration_method', integrationMethod.name,
+                     'tolerance', integrationMethod.tolerance,
+                     'interpolation_method', interpolationMethod);
+
+    result(n) = OED(responseModel, dvh_samples, options, model_param_samples{:});
+end

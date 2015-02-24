2bc9d9e84685b1cd57d795f09aa2133d70950338
Artur Szostak <artursz@iafrica.com>
2015-02-24 03:06:02 +0100

    Adding first prototype version of tools to produce mean DVH plots.

---
+++ scripts/plotMeanDVH.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+# This script produces mean DVH plots from sampled data.
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+#    Particle Therapy Project Bergen (PTPB) - tools and models for research in
+#    cancer therapy using particle beams.
+#
+#    Copyright (C) 2015 Particle Therapy Group Bergen
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# Author: Artur Szostak <artursz@iafrica.com>
+
+from __future__ import print_function
+import os
+import sys
+import argparse
+import collections
+import subprocess
+import textwrap
+
+
+class RunError(Exception):
+    """Exception class for run time errors handled by this script."""
+    pass
+
+
+def prepare_argument_parser():
+    """prepare_argument_parser() -> object
+
+    Prepares the command line argument parser.
+    """
+    argparser = argparse.ArgumentParser(
+        description = """Plots the mean DVH and uncertainty distribution from
+            sampled data produced by the sampleDVH.py tool.""")
+    argparser.add_argument("filelist", metavar = "<file>", nargs = '+',
+        help = """One or more input sampled data files.""")
+    argparser.add_argument("-O", "--outfile", dest = "outputfile",
+        default = "output.eps", metavar = "<file>", action = "store",
+        help = """The name of the output file for the plot.""")
+    argparser.add_argument("-Q", "--quantiles", dest = "quantiles",
+        default = "0:0.1:1", metavar = "<range>", action = "store",
+        help = """The quantiles binning vector. Must be in Matlab colon notation
+            for ranges or vector declaration syntax.""")
+    argparser.add_argument("-B", "--bins", dest = "bins",
+        default = "0:1:100", metavar = "<range>", action = "store",
+        help = """The dose binning points for interpolation. Must be in Matlab
+            colon notation for ranges or vector declaration syntax.""")
+    return argparser
+
+
+def run():
+    """run() -> None
+
+    Parse the command line arguments and then process the patient DVH files.
+    """
+    argparser = prepare_argument_parser()
+    args = argparser.parse_args()
+    # Prepare the Matlab script to execute, starting with the DVH file list.
+    script = ""
+    fileliststr = ", ".join(map(lambda x: "'{0}'".format(x), args.filelist))
+    script += "inputfiles = {" + fileliststr + "};\n"
+    script += "outfilename = '{0}';\n".format(args.outputfile);
+    script += "interpMethod = 'pchip';\n"
+    script += "dosebins = {0};\n".format(args.bins);
+    script += "quantilebins = {0};\n".format(args.quantiles);
+    # Add commands to load the DVHs.
+    script += textwrap.dedent("""\
+        inputfile = inputfiles{1};
+        Samples = load(inputfile, 'Samples').Samples;
+        [X, Y] = calcDVHquantiles(Samples.doses, Samples.volumebins,
+                                  dosebins, quantilebins, interpMethod);
+        plot(X, Y);
+        print('-landscape', '-color', outfilename);
+        """)
+    # Invoke octave to execute the Matlab script.
+    command_less_script = ['octave', '-q', '--path', '@@MFILE_PATH@@', '--eval']
+    command = command_less_script + [script]
+    if subprocess.call(command) != 0:
+        cmdstr = " ".join(command_less_script + ["'...'"])
+        filename = "failed_script.m"
+        with open(filename, "w") as scriptfile:
+            scriptfile.write(script)
+        raise RunError("Execution of command failed: {0}\nWrote the failing"
+                       " script that to file '{1}'.".format(cmdstr, filename))
+
+
+if __name__ == '__main__':
+    try:
+        run()
+    except RunError as e:
+        print("ERROR: " + str(e), file = sys.stderr)

383e535e249a748d0201cc8cdbf00205cda5e586
Artur Szostak <artursz@iafrica.com>
2015-12-22 23:49:59 +0100

    Restrict to 95% confidence interval for density distribution.

--- runs/relativeRiskAnalysis/makeDVHplots.sh
+++ runs/relativeRiskAnalysis/makeDVHplots.sh
@@ -4,6 +4,7 @@
 BLADDER_ORGAN_ID="Bladder_P"
 RECTUM_ORGAN_ID="Rectum_P_MT"
 N_EXTRA_CURVES=10
+QUANTILES="0.025:0.05:0.975"
 NSAMPLES=100
 BINNING="0:0.01:1"
 
@@ -18,8 +19,8 @@ applyResponseDVH.py -m '@(x) x./1.1' -V '0:0.001:1' -D '0:0.1:70' data/IMPTdvh/i
 sampleDVH.py --nsamples "$NSAMPLES" --bins "$BINNING" --organ "$BLADDER_ORGAN_ID" data/CionDataPhysicalDose/HUH*.mat -O samples_cion_bladder.mat
 sampleDVH.py --nsamples "$NSAMPLES" --bins "$BINNING" --organ "$BLADDER_ORGAN_ID" ScaledIMPT/impt*.mat -O samples_impt_bladder.mat
 sampleDVH.py --nsamples "$NSAMPLES" --bins "$BINNING" --organ "$BLADDER_ORGAN_ID" data/VMATdvh/vmat*.mat -O samples_vmat_bladder.mat
-plotMeanDVH.py --density --gnuplot --extra-curves "$N_EXTRA_CURVES" --outfile bladder.png samples_cion_bladder.mat samples_vmat_bladder.mat samples_impt_bladder.mat
+plotMeanDVH.py --density --gnuplot --quantiles "$QUANTILES" --extra-curves "$N_EXTRA_CURVES" --outfile bladder.png samples_cion_bladder.mat samples_vmat_bladder.mat samples_impt_bladder.mat
 sampleDVH.py --nsamples "$NSAMPLES" --bins "$BINNING" --organ "$RECTUM_ORGAN_ID" data/CionDataPhysicalDose/HUH*.mat -O samples_cion_rectum.mat
 sampleDVH.py --nsamples "$NSAMPLES" --bins "$BINNING" --organ "$RECTUM_ORGAN_ID" ScaledIMPT/impt*.mat -O samples_impt_rectum.mat
 sampleDVH.py --nsamples "$NSAMPLES" --bins "$BINNING" --organ "$RECTUM_ORGAN_ID" data/VMATdvh/vmat*.mat -O samples_vmat_rectum.mat
-plotMeanDVH.py --density --gnuplot --extra-curves "$N_EXTRA_CURVES" --outfile rectum.png samples_cion_rectum.mat samples_vmat_rectum.mat samples_impt_rectum.mat
+plotMeanDVH.py --density --gnuplot --quantiles "$QUANTILES" --extra-curves "$N_EXTRA_CURVES" --outfile rectum.png samples_cion_rectum.mat samples_vmat_rectum.mat samples_impt_rectum.mat
